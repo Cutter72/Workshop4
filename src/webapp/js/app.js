f87c31d376e1e0aa4fcdec454e79f2d4ca07fc91
Cutter72 <drelich_pawel@o2.pl>
2019-01-25 16:53:47 +0100

    Koniec warsztatów

--- src/webapp/js/app.js
+++ src/webapp/js/app.js
@@ -1,7 +1,5 @@
 $('document').ready(function () {
     getAllBooks();
-    getBook(2);
-
 
 });
 
@@ -25,28 +23,41 @@ function displayAll(bookList) {
         var newLi = $('<li>').text(bookList[i].title).attr('data-id', [i]+1);
         ul.append(newLi);
         var newDiv = $('<div>').attr('data-id', [i]+1).html(bookList[i].isbn+'<br/>'+
-        bookList[i].author+'<br/>'+bookList[i].publisher).hide();
+        bookList[i].author+'<br/>'+bookList[i].publisher+'<br/>'+bookList[i].type).hide();
         ul.append(newDiv);
         console.log(bookList[i]);
     }
 }
 
-function getBook(id) {
-    $.ajax({
-        url: 'http://localhost:8282/books/'+id,
-        type: 'GET',
-        dataType: 'json'
-    }).done(function (result) {
-        display(result);
-    });
-}
+// function getBook(id) {
+//     $.ajax({
+//         url: 'http://localhost:8282/books/'+id,
+//         type: 'GET',
+//         dataType: 'json'
+//     }).done(function (result) {
+//         display(result);
+//     });
+// }
 
 function display(bookId) {
     var ul = $('#book-list');
         var newLi = $('<li>').text(bookId.title).attr('data-id', bookId.id);
         ul.append(newLi);
         var newDiv = $('<div>').attr('data-id', bookId.id).html(bookId.isbn+'<br/>'+
-            bookId.author+'<br/>'+bookId.publisher).hide();
+            bookId.author+'<br/>'+bookId.publisher+'<br/>'+bookId.type).hide();
         ul.append(newDiv);
         console.log(bookId);
 }
+
+// function addBook(book) {
+//     var addBtn = $('#add-book');
+//     newBtn.on('click', function () {
+//         $.ajax({
+//             url: 'http://localhost:8282/books/'+id,
+//             type: 'DELETE',
+//             dataType: 'json'
+//         }).done(function (result) {
+//             display(result);
+//         });
+//     });
+// }
