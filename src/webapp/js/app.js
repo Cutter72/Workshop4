3dfa656e3c0e1ba73fe8ca3b1c9227cd2580b930
Cutter72 <drelich_pawel@o2.pl>
2019-01-25 15:09:33 +0100

    Zadanie 3 done

--- src/webapp/js/app.js
+++ src/webapp/js/app.js
@@ -1,21 +1,46 @@
 $('document').ready(function () {
-
-    $.ajax({
-        url: 'http://date.jsontest.com',
-        type: 'GET',
-        dataType: 'json'
-
-    }).done(function (result) {
-        console.log(result);
-    });
-
-    $.ajax({
-        url: 'https://swapi.co/api/people/4/',
-        type: 'GET',
-        dataType: 'json'
-
-    }).done(function (result) {
-        console.log(result);
+    getAllBooks();
+    var ul = $('#book-list');
+    ul.on('click', 'li', function () {
+        $(this).next().slideToggle('slow');
     });
 
 });
+
+function getAllBooks() {
+    $.ajax({
+        url: 'http://localhost:8282/books',
+        type: 'GET',
+        dataType: 'json'
+    }).done(function (result) {
+        display(result);
+    });
+    getBook(1);
+}
+
+function display(bookList) {
+    var ul = $('#book-list');
+    for (var i = 0; i < bookList.length; i++) {
+        var newLi = $('<li>').text(bookList[i].title).attr('data-id', [i]);
+        ul.append(newLi);
+        var newDiv = $('<div>').attr('data-id', [i]).html(bookList[i].isbn+'<br/>'+
+        bookList[i].author+'<br/>'+bookList[i].publisher).hide();
+        ul.append(newDiv);
+        console.log(bookList[i]);
+    }
+}
+
+function getBook(id) {
+    $.ajax({
+        url: 'http://localhost:8282/books/'+id,
+        type: 'GET',
+        dataType: 'json'
+    }).done(function (result) {
+        showMore(result);
+        console.log(result);
+    });
+}
+
+function showMore(book) {
+
+}
